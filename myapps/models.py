c0788955376637015b54c8971b2fc6a34393347d
leeyoungjoo <youngjoo6452@gmail.com>
2025-01-20 20:48:17 +0900

    create models

--- myapps/models.py
+++ myapps/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Article(models.Models):
-    title = models.CharField()
-    content = 
-    created_at = 
+class Article(models.Model):  
+    title = models.CharField(max_length =255 ) #최대 길이 설정
+    content = models.TextField() #텍스트 필드 추가 
+    created_at = models.DateTimeField(auto_now_add=True) #자동 생성 날짜/시간 추가
+
+    def __str__(self):
+        return self.title #객체를 문자열로 변환 하면 제목을 반환.    
